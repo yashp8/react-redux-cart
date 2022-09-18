d40fe86d03920d74ff3538698e90f45c0b68cd98
mowglie <Yash.Patel@yewtechnologies.co.uk>
2022-09-18 11:36:04 +0100

    initial commit

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,38 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My Book',
+    description: 'bjhlcbjlh kjjkb',
+  },
+  {
+    id: 'p2',
+    price: 12,
+    title: 'My  2 Book',
+    description: 'bjhlcbjlh kjjkb',
+  },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            id={product.id}
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
